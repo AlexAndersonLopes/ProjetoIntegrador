1c06873d318fcc62f592eeee40d571d2fcc19211
AlexAndersonLopes <124632986+AlexAndersonLopes@users.noreply.github.com>
2023-05-09 23:46:28 -0300

    Versão pronta para Correção

--- src/jdbc/ConnectFactory.java
+++ src/jdbc/ConnectFactory.java
@@ -9,14 +9,14 @@ public class ConnectFactory {
 
         try {
             //return DriverManager.getConnection("jdbc:mysql://34.95.190.240/programaProjetoIntegrador","root","G#e\\{;#juN'B4sSy");        
-            //return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/PI_SistemaVendas", "root", "123456");
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/PI_SistemaVendas", "root", "123456");
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
+           /* Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://34.95.190.240/programaProjetoIntegrador";
             String user = "Alex";
             String password = "123";
             Connection connection = DriverManager.getConnection(url, user, password);
-            return connection;
+            return connection;*/
 
         } catch (Exception e) {
             throw new RuntimeException(e);
